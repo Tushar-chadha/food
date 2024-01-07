ae081d1797ca531785901a59cad5accb62f56684
tushar-chadha <tusharchadha26@gmail.com>
2024-01-07 11:32:01 +0530

    changewd popularProductScreen

--- lib/screens/food/popularScreenDetails.dart
+++ lib/screens/food/popularScreenDetails.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:foody/helper/routes/routeHelper.dart';
 import 'package:foody/model/productModel.dart';
-import 'package:foody/screens/home/homepage.dart';
 import 'package:foody/utils/colors.dart';
 import 'package:foody/utils/dimensions.dart';
-import 'package:foody/widgets/appIcon.dart';
 import 'package:foody/widgets/bigtext.dart';
 import 'package:foody/widgets/extendableText.dart';
-import 'package:foody/widgets/infoColumn.dart';
+import 'package:foody/widgets/navigatorRow.dart';
 import 'package:gap/gap.dart';
-import 'package:get/get.dart';
 
 class popularFoodDetails extends StatefulWidget {
   @override
@@ -27,86 +23,55 @@ class _popularFoodDetailsState extends State<popularFoodDetails> {
       backgroundColor: Colors.white,
       body: SafeArea(
         top: false,
-        child: Stack(children: [
-          Positioned(
-            left: 0,
-            right: 0,
-            child: Container(
-              width: double.maxFinite,
-              height: dimensions.popularImageHeight,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/image/food0.png"),
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              forceElevated: true,
+              flexibleSpace: const FlexibleSpaceBar(
+                  background: Image(
+                      image: AssetImage("assets/image/food0.png"),
+                      fit: BoxFit.cover)),
+              elevation: 20,
+              pinned: true,
+              expandedHeight: dimensions.height300,
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(dimensions.height30),
+                child: Container(
+                  padding: const EdgeInsets.only(top: 15),
+                  alignment: Alignment.bottomCenter,
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(dimensions.radius20),
+                        topRight: Radius.circular(dimensions.radius20)),
+                  ),
+                  child: BigText(
+                      size: dimensions.height20,
+                      text: "My food is my Food",
+                      fo: FontWeight.w700),
                 ),
               ),
-            ),
-          ),
-          Positioned(
-            left: dimensions.width20,
-            top: dimensions.height20 + 30,
-            right: dimensions.width20,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(
-                  onTap: () => Get.toNamed(routeHelper.getInitial()),
-                  child: const appIcon(
-                    iconName: CupertinoIcons.arrow_left,
+              backgroundColor: const Color.fromRGBO(255, 211, 92, 1),
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 10,
                   ),
-                ),
-                const appIcon(
-                  iconName: CupertinoIcons.shopping_cart,
-                ),
+                  child: NavigatorRowIcon(),
+                )
               ],
             ),
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            top: dimensions.popularImageHeight - 30,
-            child: Container(
-                padding: EdgeInsets.only(
-                    left: dimensions.width20,
-                    right: dimensions.width20,
-                    top: dimensions.height20),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: const [
-                    BoxShadow(
-                        offset: Offset(0, -5),
-                        blurRadius: 10,
-                        spreadRadius: 1,
-                        color: Colors.black12),
-                  ],
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(
-                      dimensions.radius30,
-                    ),
-                    topRight: Radius.circular(
-                      dimensions.radius30,
-                    ),
-                  ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.all(dimensions.height15),
+                child: extendableText(
+                  Text: Textx + Textx + Textx,
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const appColumn(
-                      text1: "Chinese Food",
-                    ),
-                    Expanded(
-                      child: SingleChildScrollView(
-                        child: extendableText(
-                          Text: Textx + Textx + Textx,
-                        ),
-                      ),
-                    ),
-                  ],
-                )),
-          ),
-        ]),
+              ),
+            )
+          ],
+        ),
       ),
       bottomNavigationBar: Container(
         height: 120,
@@ -171,3 +136,86 @@ class _popularFoodDetailsState extends State<popularFoodDetails> {
     );
   }
 }
+    /*  Stack(
+              children: [
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    width: double.maxFinite,
+                    height: dimensions.popularImageHeight,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/image/food0.png"),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: dimensions.width20,
+                  top: dimensions.height20 + 30,
+                  right: dimensions.width20,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () => Get.toNamed(routeHelper.getInitial()),
+                        child: const appIcon(
+                          iconName: CupertinoIcons.arrow_left,
+                        ),
+                      ),
+                      const appIcon(
+                        iconName: CupertinoIcons.shopping_cart,
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  top: dimensions.popularImageHeight - 30,
+                  child: Container(
+                      padding: EdgeInsets.only(
+                          left: dimensions.width20,
+                          right: dimensions.width20,
+                          top: dimensions.height20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                              offset: Offset(0, -5),
+                              blurRadius: 10,
+                              spreadRadius: 1,
+                              color: Colors.black12),
+                        ],
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(
+                            dimensions.radius30,
+                          ),
+                          topRight: Radius.circular(
+                            dimensions.radius30,
+                          ),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const appColumn(
+                            text1: "Chinese Food",
+                          ),
+                          Expanded(
+                            child: SingleChildScrollView(
+                              child: extendableText(
+                                Text: Textx + Textx + Textx,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
+                ),
+              ],
+            ),*/
+            
